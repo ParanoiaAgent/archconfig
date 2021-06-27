096c7471e30c3e35a424f032e70682a035b07606
Paranoia Agent <86332994+ParanoiaAgent@users.noreply.github.com>
2021-06-26 22:16:30 -0300

    Update archconfig.sh

--- archconfig.sh
+++ archconfig.sh
@@ -8,17 +8,19 @@ echo "Finished System Update"
 ### YAY Install
 sudo pacman -S git --noconfirm
 
-mkdir -p Downloads/Git
+cd
+sudo mkdir -p Downloads/Git
 cd Downloads/Git
 
 sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R paranoia-agent:users ./yay
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 
 echo "Finished YAY Install"
 
 ### Pacman Packages
-sudo pacman -S bspwm sxhkd playerctl rofi dunst sddm xfce4-power-manager xfce4-settings firefox kitty thunar thunar-archive-plugin gvfs-mtp udevil ranger w3m ffmpegthumbnailer flameshot starship python-pywal feh zathura zathura-pdf-poppler neofetch pulseaudio pulseaudio-alsa pavucontrol polkit-gnome lxappearance-gtk3 mousepad vim htop wget iwd wireless_tools wpa_supplicant xdg-utils xdg-user-dirs noto-fonts noto-fonts-cjk --noconfirm
+sudo pacman -S bspwm sxhkd playerctl rofi dunst xorg sddm xfce4-power-manager xfce4-settings firefox kitty thunar thunar-archive-plugin gvfs-mtp udevil ranger w3m ffmpegthumbnailer flameshot starship python-pywal feh zathura zathura-pdf-poppler neofetch pulseaudio pulseaudio-alsa pavucontrol polkit-gnome lxappearance-gtk3 mousepad vim htop wget iwd wireless_tools wpa_supplicant xdg-utils xdg-user-dirs xf86-input-wacom noto-fonts noto-fonts-cjk --noconfirm
 
 echo "Finished Installing Pacman Packages"
 
